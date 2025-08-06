afdb42cd083cb76c7811dbf7a3b8e0c323844a29
nawar123456 <84979634+nawar123456@users.noreply.github.com>
2025-08-06 22:17:06 +0300

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,8 @@
+# serializers.py
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import User, CardDetail, Transaction, DeliveryLocation, DeliverySchedule, Employee, TransferTransaction
+from .models import User, CardDetail, Transaction, DeliveryLocation, DeliverySchedule, Employee
 
 User = get_user_model()
 
@@ -10,7 +12,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'password', 'phone_number', 'birth_date', 'emirates_id', 'passport', 'status']
+        fields = [
+            'id', 'first_name', 'last_name', 'username', 'email',
+            'password', 'phone_number', 'birth_date', 'emirates_id',
+            'passport', 'status'
+        ]
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -49,65 +55,58 @@ class TransactionSerializer(serializers.ModelSerializer):
         queryset=CardDetail.objects.all(),
         write_only=True
     )
+    recipient_id = serializers.IntegerField(write_only=True, required=False)
 
     class Meta:
         model = Transaction
         fields = [
-            'transaction_type', 'amount', 'currency_from', 'currency_to',
-            'card_id', 'delivery_locations', 'delivery_schedules'
+            'transaction_type',
+            'amount',
+            'currency_from',
+            'currency_to',
+            'card_id',
+            'recipient_id',
+            'message_to_recipient',
+            'delivery_locations',
+            'delivery_schedules'
         ]
 
     def create(self, validated_data):
+        # استخراج الحقول الإضافية
         locations_data = validated_data.pop('delivery_locations', [])
         schedules_data = validated_data.pop('delivery_schedules', [])
         card_id = validated_data.pop('card_id')
+        recipient_id = validated_data.pop('recipient_id', None)
         user = self.context['request'].user
 
+        # التحقق من أن البطاقة تخص المستخدم
         if card_id.user != user:
             raise serializers.ValidationError("البطاقة لا تخصك.")
 
+        # التحقق من أن المستلم موجود (إذا كان نوع المعاملة تحويل)
+        recipient = None
+        if validated_data['transaction_type'] in ['send_money', 'receive_money']:
+            if not recipient_id:
+                raise serializers.ValidationError("حقل 'recipient_id' مطلوب للتحويلات.")
+            try:
+                recipient = User.objects.get(id=recipient_id)
+            except User.DoesNotExist:
+                raise serializers.ValidationError("المستخدم المستلم غير موجود.")
+
+        # إنشاء المعاملة
         transaction = Transaction.objects.create(
             user=user,
             card=card_id,
+            recipient=recipient,
             **validated_data
         )
 
+        # إنشاء مواقع التسليم
         for loc_data in locations_data:
             DeliveryLocation.objects.create(transaction=transaction, **loc_data)
 
+        # إنشاء جداول التسليم
         for sched_data in schedules_data:
             DeliverySchedule.objects.create(transaction=transaction, **sched_data)
 
         return transaction
-
-
-class TransferTransactionSerializer(serializers.ModelSerializer):
-    recipient_id = serializers.IntegerField(write_only=True)
-
-    class Meta:
-        model = TransferTransaction
-        fields = [
-            'transaction_type', 'recipient_id', 'amount_sent',
-            'currency_sent', 'amount_received', 'currency_received',
-            'message_to_recipient', 'location'
-        ]
-
-    def create(self, validated_data):
-        recipient_id = validated_data.pop('recipient_id')
-        user = self.context['request'].user
-
-        try:
-            recipient = User.objects.get(id=recipient_id)
-        except User.DoesNotExist:
-            raise serializers.ValidationError("المستخدم غير موجود.")
-
-        validated_data['user'] = user
-        validated_data['recipient'] = recipient
-        return super().create(validated_data)
-
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = ['id', 'first_name', 'last_name', 'role']
-        read_only_fields = ['role']
